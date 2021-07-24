cc5d8d5683068b5a870969ee8e478cd687e0a7ca
Muhammad Idrees <midexigner@gmail.com>
2021-07-25 00:13:57 +0500

    removed config plugins.js cloudinary credential

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
     upload: {
       provider: 'cloudinary',
       providerOptions: {
-        cloud_name: 'mi-dexigner',
-        api_key: '449583657825885',
-        api_secret: 'dSy_AeyMIKz7GC1F2pPtzYCOLqg',
+        cloud_name: '',
+        api_key: '',
+        api_secret: '',
       },
     },
     // ...
