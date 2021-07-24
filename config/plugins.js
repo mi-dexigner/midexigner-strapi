ecfd4d2cabebbd565680e6e071a85b936a33ae7f
muhammad.idrees <midexigner@gmail.com>
2021-07-24 18:48:15 +0500

    woking on post and also add on cloudniary image upload

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
     upload: {
       provider: 'cloudinary',
       providerOptions: {
-        cloud_name: env('mi-dexigner'),
-        api_key: env('449583657825885'),
-        api_secret: env('dSy_AeyMIKz7GC1F2pPtzYCOLqg'),
+        cloud_name: 'mi-dexigner',
+        api_key: '449583657825885',
+        api_secret: 'dSy_AeyMIKz7GC1F2pPtzYCOLqg',
       },
     },
     // ...
